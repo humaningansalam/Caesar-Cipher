5a6543c12dc6804c31e645381c3cb32e6a0580fc
sh9634 <sanghun9634@gmail.com>
2023-05-02 02:24:50 +0900

    test: Test Caesar Unit test
    input alphabet Caesar encrypt decrypt test

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,1 +1,20 @@
-print('hello')
+
+
+
+alpha = input('알파벳을 입력해주세요.').upper()
+list = []
+for i in alpha:
+    list.append((ord(i)-64)+3)
+    #print((ord(i)-64)+3, end=' ')
+print(list)
+
+for i in list:
+    print(chr(i+64), end=' ')
+de_list = []
+for i in list:
+    print(chr(i+64-3), end=' ')
+
+
+
+
+
