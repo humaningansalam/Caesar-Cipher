0a2a17c1663c46106a527e3368e9e73d6a30ddc3
yn9634 <sanghun9634@gmail.com>
2023-05-02 11:23:47 +0900

    feat: Define encrypt and decrypy

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,20 +1,36 @@
+def encrypt():
+    list = []
+    alpha = input('알파벳을 입력해주세요.').upper()
 
+    for i in alpha:
+        list.append((ord(i)-64)+3)
+    for i in list:
+        print(chr(i+64), end=' ')
+    print('\n')
 
+def decrypt():
+    list = []
+    alpha = input('알파벳을 입력해주세요.').upper()
 
-alpha = input('알파벳을 입력해주세요.').upper()
-list = []
-for i in alpha:
-    list.append((ord(i)-64)+3)
-    #print((ord(i)-64)+3, end=' ')
-print(list)
+    for i in alpha:
+        list.append((ord(i)-64)-3)
+    for i in list:
+        print(chr(i+64), end=' ')
+    print('\n')
 
-for i in list:
-    print(chr(i+64), end=' ')
-de_list = []
-for i in list:
-    print(chr(i+64-3), end=' ')
+if __name__ == "__main__":
+    print('작업을 선택하세요')
 
+    while True:
+        i = input('1.암호화 2.복호화 3.종료')
 
-
+        if i == '1':
+            encrypt()
+        elif i == '2':
+            decrypt()
+        elif i == '3':
+            break
+        else:
+            print('잘못된 입력입니다')
 
 
